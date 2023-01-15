764ee19d07ba79fe91b9d653c4cf31a2f7389432
Lomakin Semen <lomakin.semyon.olegovich@gmail.com>
2023-01-15 22:24:57 +0200

    Implemented settings config

---
+++ Assets/Factory Game/Scripts/GameplaySettingsData.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay Settings Data")]
+public class GameplaySettingsData : ScriptableObject
+{
+    [SerializeField] 
+    private List<GameplaySettingsPreset> settingsConfig;
+
+    private Dictionary<GameDifficulty, GameplaySettings> cachedData = new();
+
+    private void OnValidate() => CacheSerializeData();
+
+    private void CacheSerializeData()
+    {
+        cachedData.Clear();
+
+        foreach (var item in settingsConfig)
+        {   
+            if (!cachedData.ContainsKey(item.difficulty))
+                cachedData.Add(item.difficulty, item.gameplaySettings);
+        }
+    }
+
+    public GameplaySettings GetGameplaySettings(GameDifficulty difficulty)
+    {
+        bool containsRequestedSettings;
+        containsRequestedSettings = cachedData.TryGetValue(difficulty, out GameplaySettings result);
+        
+        return containsRequestedSettings ? result : null;
+    }
+}
+
+[System.Serializable]
+public class GameplaySettingsPreset
+{
+    public GameDifficulty difficulty;
+    [Space]
+    public GameplaySettings gameplaySettings;
+}
+
+[System.Serializable]
+public class GameplaySettings
+{
+    public int timeLimit;
+    public int requiredRepairs;
+    public int spawnLimit;
+    public Vector2 spawnIntervalRange;
+}
